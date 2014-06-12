82259039c19b56254cfde3e77d7fc10928ff025a
Yorick Peterse <yorickpeterse@olery.com>
2014-06-12 13:47:23 +0200

    Use RSpec 3.0.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,6 @@
 require_relative '../../lib/opener/pos_taggers/it'
 require 'tempfile'
+require 'rspec'
 
 def kernel_root
   File.expand_path("../../../", __FILE__)
@@ -7,4 +8,14 @@ end
 
 def kernel
   Opener::POSTaggers::IT.new
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
